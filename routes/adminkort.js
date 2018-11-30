6c23d7a3f085c5c58fffad8a4dda1b071c1c166d
neno2000 <duran.ernesto@gmail.com>
2018-11-30 12:21:36 +0100

    Util object added
    Util object added

--- routes/adminkort.js
+++ routes/adminkort.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var lUtility require('../model/util');
+var lUtility = require('./../model/util');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
